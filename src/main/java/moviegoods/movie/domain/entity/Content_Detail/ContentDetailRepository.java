9b988a70ca1b2dc0a4f217a5fabcba1b1c6eae5d
redhi <redji81@naver.com>
2022-03-01 20:28:44 +0900

    [#28]feat: JSESSIONID 관련 코드 구현 및 서버 코드 통합

--- src/main/java/moviegoods/movie/domain/entity/Content_Detail/ContentDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package moviegoods.movie.domain.entity.Content_Detail;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface ContentDetailRepository extends JpaRepository<Content_Detail, Long> {
-    Optional<Content_Detail> findById(Long content_detail_id);
-}
--- src/main/java/moviegoods/movie/domain/entity/Transaction/Status.java
+++
@@ -1,5 +0,0 @@
-package moviegoods.movie.domain.entity.Transaction;
-
-public enum Status {
-    진행중, 마감
-}
